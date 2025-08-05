ed07de1ea578f4ac7998e3c7c6cc8216a8def523
alexeugenekotoka <aekotoka@st.ug.edu.gh>
2025-08-05 13:22:10 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,39 @@
 using System;
 
-class Animal
+abstract class Shape
 {
-    public virtual void MakeSound()
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
     {
-        Console.WriteLine("Some generic sound");
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
     }
 }
 
-class Dog : Animal
+class Rectangle : Shape
 {
-    public override void MakeSound()
-    {
-        Console.WriteLine("Bark");
-    }
-}
+    public double Width { get; set; }
+    public double Height { get; set; }
 
-class Cat : Animal
-{
-    public override void MakeSound()
+    public Rectangle(double width, double height)
     {
-        Console.WriteLine("Meow");
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
     }
 }
 
@@ -28,12 +41,10 @@ class Program
 {
     static void Main()
     {
-        Animal genericAnimal = new Animal();
-        Animal dog = new Dog();
-        Animal cat = new Cat();
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
 
-        genericAnimal.MakeSound();
-        dog.MakeSound();
-        cat.MakeSound();
+        Console.WriteLine("Circle Area: " + circle.GetArea());
+        Console.WriteLine("Rectangle Area: " + rectangle.GetArea());
     }
 }
